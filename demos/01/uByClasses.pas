5d3fc292ea85b05b5c5298a586b34087903a8d9b
Laurent Meyer <lmeyer@ea4d.com>
2024-02-28 10:16:12 +0100

    Demo WIP

--- demos/01/uByClasses.pas
+++ demos/01/uByClasses.pas
@@ -7,7 +7,7 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.TypInfo,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.Memo.Types,
   FMX.ScrollBox, FMX.Memo
-  {$IFNDEF JSX2_NOVAR}
+  {$IFNDEF JSX_NOVAR}
   , System.Variants
   {$ENDIF}
   , System.Generics.Collections
@@ -17,92 +17,117 @@ uses
   , W3DJsonX2.RTTI
   , W3DJsonX2.Types
   , W3DJsonX2.Utils
-  , W3DCloneable
+  , W3DCloneable, FMX.Layouts
   ;
 
 type
+
+  TJSX2ScannerCallback = reference to procedure;
+
   TForm2 = class(TForm)
     Button1: TButton;
     Memo1: TMemo;
+    Layout1: TLayout;
+    Layout2: TLayout;
+    Memo2: TMemo;
+    Layout3: TLayout;
+    Memo3: TMemo;
+    Memo4: TMemo;
     procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
-    procedure Log(Str: string);
   end;
 
-  ISimpleObject = interface(IInterface) ['{D259E2E2-2EDD-456D-A4F2-4AD9AFA6494C}'] end;
 
 
-  TSimpleObject = class(TIJX2, ISimpleObject)
-  private
-    var1: TValue;
-  public
-    var2: TValue;
-    procedure SetVar1(v: TValue);
-    function GetVar1: TValue;
 
-    property PVar1: TValue read GetVar1 write SetVar1;
-  end;
 
-  TSimpleObject2 = Class(TIJX2);
-
-  TComplexObj = class(TJX2)
-  public
 
 //----------------------------------------------------------------------------//
+// Json Objectx Definition :
+
+
+  // A really simple container
+ TSimpleSubObject = Class(TJX2)
+    var3: TValue;
+    var4: TValue;
+  end;
+
+  // Interface on a TSimpleObject
+  ISimpleObject = interface(IInterface) ['{D259E2E2-2EDD-456D-A4F2-4AD9AFA6494C}'] end;
+
+  // a simple object with property and sub. object
+  // ALL OBJECTS AND INTERFACED OBJECTS ARE OWNED
+  // It means tha that you don't have to take care of their destruvtion (freeing)
+  TSimpleObject = class(TIJX2, ISimpleObject)                                   // TSimple Object, must inherit from TIJX2 if interfaced
+  private
+    [JX2AttrName('var1')]                                                       // Fvar1 field name will be mapped as json "var" name
+    Fvar1: TValue;                                                              // Fvar1 varianle
+  public
+    var2: TValue;                                                               //  a TValue var2 variable
+    Obj: TSimpleSubObject;                                                      // an Object (TObject)
+    procedure SetVar1(v: TValue);                                               // Settet
+    function GetVar1: TValue;                                                   // Getter
+    property Var1: TValue read GetVar1 write SetVar1;                           // Property Var1
+  end;
+
+  // A complex object definitionr
+  TComplexObj = class(TJX2)                                                     // inherit from TJX2)
+  public
 
     // The fields order does not matter
 
-    valNull: TValue;
-    valString: TValue;
-    valInteger: TValue;
-{$IFNDEF JSX2_NOVAR}
-    valVariantString: Variant;
-    valBoolean: Variant;
-    valDouble: Variant;
-{$ENDIF}
-    valDateTime: variant;
-    valUTCDateTime: variant; // ISO 8601 UTC Date string
-    [JX2AttrName('type')]
-    valType: TValue;
+    valNull: TValue;                                                            // null value
+    valString: TValue;                                                          // string
+    valInteger: TValue;                                                         // Integer
+    {$IFNDEF JSX_NOVAR}
+    valVariantString: Variant;                                                  // variant: string;
+    valBoolean: Variant;                                                        // Boolean
+    valDouble: Variant;                                                         // Double
+    valDateTime: variant;                                                       // DateTime in fact Double
+    valUTCDateTime: variant;                                                    // DateTime ISO 8601 UTC, standard
+    {$ENDIF}
+   [JX2AttrName('type')]
+    valType: TValue;                                                             // A TValue 'valType' mapped to Json 'type'
 
-    // Object / Interface
+                                                                                // Object and Interface definition
     ObjectType: TSimpleObject;
-    [JX2AttrClass(TSimpleObject)]
+    [JX2AttrClass(TSimpleObject)]                                               // The interfaced object of the interface
     IntfType: IJX2;
 
-    //List of values;
-{$IFNDEF JSX2_NOVAR}
-    VaRList: TJX2VarList;
-{$ENDIF}
-    VaLlist: TJX2ValueList;
-{$IFNDEF JSX2_NOVAR}
-    [JX2AttrClass(TIJX2VaRList)]
-    IVaRlist: IJX2VaRList;
-{$ENDIF}
-    [JX2AttrClass(TIJX2ValueList)]
-    IVaLlist: IJX2ValueList;
+    {$IFNDEF JSX_NOVAR}                                                                            //List of values (array) :
+    VaRList: TJX2VarList;                                                       // variant list
+    {$ENDIF}
+    VaLlist: TJX2ValueList;                                                     // TValue List
 
-    // List of Object;
+
+    {$IFNDEF JSX_NOVAR}
+    [JX2AttrClass(TIJX2VaRList)]                                                // Object mapped to the variant list interface
+    IVaRlist: IJX2VaRList;
+    {$ENDIF}                                                      // Interfaced variant list
+    [JX2AttrClass(TIJX2ValueList)]                                              // Object mapped to the TValue list interface
+    IVaLlist: IJX2ValueList;                                                    // Interfaced TValue list
+
+    // List of Objects;
     [JX2AttrClass(TSimpleObject)]
     ObjList: TJX2ObjList;
-    [JX2AttrClass(TIJX2ObjList, TSimpleObject)]
+    [JX2AttrClass(TIJX2ObjList, TSimpleObject)]                                 // Define the Interface Object of IObjList and the contained one.
     IObjList: IJX2ObjList;
 
     // Dictionary of string/Values
-{$IFNDEF JSX2_NOVAR}
-    StrVarDic: TJX2StrVarDic;
-{$ENDIF}
-    StrValueDic: TJX2StrValueDic;
+    {$IFNDEF JSX_NOVAR}
+    StrVarDic: TJX2StrVarDic;                                                   // a string/variant dictionnary
+    {$ENDIF}
+    StrValueDic: TJX2StrValueDic;                                               // a string/TValue dictionnary
     [JX2AttrClass(TSimpleObject)]
-    StrObjDic: TJX2StrObjDic;
-{$IFNDEF JSX2_NOVAR}
-    [JX2AttrClass(TIJX2StrVarDic, TSimpleObject)]
-    IStrVarDic: IJX2StrVarDic;
-{$ENDIF}
-    [JX2AttrClass(TIJX2StrValueDic, TSimpleObject)]
+    StrObjDic: TJX2StrObjDic;                                                   // a string/TObject dictionnary
+    {$IFNDEF JSX_NOVAR}
+    [JX2AttrClass(TIJX2StrVarDic)]
+    IStrVarDic: IJX2StrVarDic;                                                  //Same with interfaced object
+    {$ENDIF}
+    [JX2AttrClass(TIJX2StrValueDic)]
     IStrValueDic: IJX2StrValueDic;
     [JX2AttrClass(TIJX2StrObjDic, TSimpleObject)]
     IStrObjDic: IJX2StrObjDic;
@@ -115,11 +140,6 @@ implementation
 uses DateUtils;
 
 {$R *.fmx}
-
-procedure TForm2.Log(Str: string);
-begin
-  Memo1.Lines.Add(Str);
-end;
 
 procedure TForm2.Button1Click(Sender: TObject);
 var
@@ -139,60 +159,79 @@ var
 
 begin
 
+  Memo1.Lines.Clear;
+  Memo2.Lines.Clear;
+  Memo3.Lines.Clear;
+
 //----------------------------------------------------------------------------//
 
-  // Create a Json Object
+  // Create a Json Object, fill the object
   Obj := TComplexObj.Create;
 
   //Primitives
 
-  Obj.valNull := nil;
-  Obj.valString := 'ooŘaa鱇bb😃cc';
-  Obj.valInteger := 15;
-{$IFNDEF JSX2_NOVAR}
-  Obj.valVariantString := 'bbb';
-  Obj.valBoolean := False;
-  Obj.valDouble := 2.2;
-{$ENDIF}
-
-  Obj.valDateTime := Double(Now);
-  Obj.valUTCDateTime := DateToIso8601( TTimeZone.Local.ToUniversalTime(Now) );
-  Obj.valType := 'erroneous delphi field name...';
+  Obj.valNull := nil; // a Null (nil) value
+  Obj.valString := 'Value : ooŘaa鱇bb😃cc'; // a string UTF8
+  Obj.valInteger := 15; // an Integer
+  {$IFNDEF JSX_NOVAR}
+  Obj.valVariantString := 'Variant : ooŘaa鱇bb😃cc'; // variant string
+  Obj.valBoolean := False;  // Boolean value
+  Obj.valDouble := 2.2;     // Double value
+  Obj.valDateTime := Double(Now); // Datetime: double value
+  Obj.valUTCDateTime := DateToIso8601( TTimeZone.Local.ToUniversalTime(Now) ); // an UTC ISO8601 DateTime (string);
+  {$ENDIF}
+  Obj.valType := 'erroneous delphi field name...'; // string of a renamed field
 
   // Object/Interface
 
   Obj.ObjectType := TSimpleObject.Create;
   Obj.ObjectType.var1 := 'a var 1 Object';
   Obj.ObjectType.var2 := 'a var 2 Object';
+  Obj.ObjectType.Obj := TSimpleSubObject.Create;
+  Obj.ObjectType.Obj.var3 := '3';
+  Obj.ObjectType.Obj.var4 := '4';
+  TJX2(Obj.ObjectType.Clone).Free;
+
+
+  Obj.IntfType := TSimpleObject( Obj.ObjectType.Clone);     // Use cloning for copying previous object, Inteface
+  TSimpleObject(Obj.IntfType).var1 := 'a var 5 Intf';       // Set values :
+  TSimpleObject(Obj.IntfType).var2 := 'a var 6 Intf';
+  TSimpleObject(Obj.IntfType).Obj.var3 := 'a var 7 Intf';   // Set new object values :
+  TSimpleObject(Obj.IntfType).Obj.var4 := 'a var 8 Intf';
 
   Obj.IntfType := TSimpleObject.Create;
   TSimpleObject(Obj.IntfType).var1 := 'a var 3 Intf';
   TSimpleObject(Obj.IntfType).var2 := 'a var 4 Intf';
-
+  TJX2(Obj.IntfType.Clone).Free;
+                                                             // Testing Object Clone
   // Lists (arrays) of Var/Value/Obj
 
-{$IFNDEF JSX2_NOVAR}
+  {$IFNDEF JSX_NOVAR}
   Obj.VaRList := TJX2VarList.Create;
   Obj.VaRList.Add(1);   Obj.VaRList.Add(2);
-{$ENDIF}
+  {$ENDIF}
   Obj.VaLlist := TJX2ValueList.Create;
   Obj.VaLlist.Add(3);   Obj.VaLlist.Add(4);
+  Obj.VaLlist.Clone.Free;
 
   Obj.ObjList := TJX2ObjList.Create;
   Simple := TSimpleObject.Create;
   Simple.var1 := 5;
   Simple.var2 := 'five';
   TJX2ObjList(Obj.ObjList).Add(Simple);
+  Obj.ObjList.Clone.Free;
 
-{$IFNDEF JSX2_NOVAR}
+  {$IFNDEF JSX_NOVAR}
   Obj.IVaRList := TIJX2VarList.Create;
   TIJX2VaRList(Obj.IVaRList).Add('6A');
   TIJX2VaRList(Obj.IVaRList).Add('7B');
+  TIJX2(TIJX2VaRList(Obj.IVaRList).Clone).Free;
+  {$ENDIF}
 
   Obj.IVaLList := TIJX2ValueList.Create;
   TIJX2ValueList(Obj.IVaLList).Add('8C');
   TIJX2ValueList(Obj.IVaLList).Add('9D');
-{$ENDIF}
+  TIJX2(TIJX2ValueList(Obj.IVaLList).Clone).Free;
 
   Obj.IObjList := TIJX2ObjList.Create;
   Simple := TSimpleObject.Create;
@@ -203,62 +242,71 @@ begin
   Simple.var1 := 20;
   Simple.var2 := 'twenny';
   TIJX2ObjList(Obj.IObjList).Add(Simple);
+  TIJX2(TIJX2ObjList(Obj.IObjList).Clone).Free;
 
   // Dictionnaries Var/Values/Object
-{$IFNDEF JSX2_NOVAR}
-  Obj.StrVarDic := TJX2StrVarDic.Create;
+  {$IFNDEF JSX_NOVAR}
+  ObjeStrVarDic := TJX2StrVarDic.Create;
   Obj.StrVarDic.Add('1','A');
   Obj.StrVarDic.Add('2', 12);
-{$ENDIF}
+  Obj.StrVarDic.Clone.Free;
+  {$ENDIF}
 
   Obj.StrValueDic := TJX2StrValueDic.Create;
   Obj.StrValueDic.Add('1', 7);
   Obj.StrValueDic.Add('2', 13);
+  Obj.StrValueDic.Clone.Free;
 
   Obj.StrObjDic := TJX2StrObjDic.Create;
   Simple := TSimpleObject.Create;
   Simple.var1 := 'ValueKey';
-  Simple.var2 := 'ObjValue';
+  Simple.var2 := 'ObjValue ''V2/''/ escaped';
   Obj.StrObjDic.Add('Key1', Simple);
-{$IFNDEF JSX2_NOVAR}
+  Obj.StrObjDic.Clone.Free;
+
+  {$IFNDEF JSX_NOVAR}
   Obj.IStrVarDic := TIJX2StrVarDic.Create;
   TIJX2StrVarDic(Obj.IStrVarDic).Add('3','B');
   TIJX2StrVarDic(Obj.IStrVarDic).Add('4', 13);
-{$ENDIF}
+  TIJX2(TIJX2StrVarDic(Obj.IStrVarDic).Clone).Free;
+  {$ENDIF}
+
   Obj.IStrValueDic := TIJX2StrValueDic.Create;
   TIJX2StrValueDic(Obj.IStrValueDic).Add('3','D');
   TIJX2StrValueDic(Obj.IStrValueDic).Add('4', 14);
+  TIJX2(TIJX2StrValueDic(Obj.IStrValueDic).Clone).Free;
 
   Obj.IStrObjDiC := TIJX2StrObjDic.Create;
   Simple := TSimpleObject.Create;
   Simple.var1 := 'V1';
   Simple.var2 := 'V2';
   TIJX2StrObjDic(Obj.IStrObjDiC).Add('SimpleX', Simple);
+  TIJX2(TIJX2StrObjDic(Obj.IStrObjDiC).Clone).Free;
 
 //----------------------------------------------------------------------------//
 
   // Obj Serialization
   Json := W3DJSX2.Serialize(Obj, [jxoReturnEmptyObject]);
   Memo1.Lines.Add( 'Object:');
-  Memo1.Lines.Add( Json );
+  Memo1.Lines.Add( Json + '    Lenght: ' + Length(Json).ToString);
 
   // Cloning through serialization
   CloneObj := W3DJSX2.Deserialize<TComplexObj>(Json);
   Json := W3DJSX2.Serialize(CloneObj);
-  Log( 'Cloned Object (Json Des/Ser):');
-  Log( Json );
+  Memo1.Lines.Add( 'Cloned Object (Json Des/Ser):');
+  Memo1.Lines.Add( Json + '    Lenght: ' + Length(Json).ToString);
   CloneObj.Free;
 
   // Native CLoning
   CloneObj := TComplexObj(Obj.Clone);
-  Log( 'Natively Cloned Object:');
-  Log( W3DJSX2.Serialize(CloneObj) );
+  Memo1.Lines.Add( 'Natively Cloned Object:');
+  Json := W3DJSX2.Serialize(CloneObj);
+  Memo1.Lines.Add( Json + '    Lenght: ' + Length(Json).ToString);
   CloneObj.Free;
 
   JsonBeauty := JsonBeautifier(Json);
-  Log('');
-  Log('Json Beautifier :');
-  Log(JsonBeauty);
+  Memo2.Lines.Add('Json Beautifier :');
+  Memo2.Lines.Add(JsonBeauty);
 
   FS := TFileStream.Create('Beauty.json', fmCreate);
   FS.WriteRawUTF8String(UTF8String(JsonBeauty));
@@ -266,58 +314,60 @@ begin
 
 //----------------------------------------------------------------------------//
 
-  Log('');
-  Log('String (TValue): ' + Obj.valString.AsString);
-{$IFNDEF JSX2_NOVAR}
-  Log('String (Variant): ' + Obj.valVariantString);
-{$ENDIF}
-  Log('Double: ' + FloatToStr(Obj.valDouble));
+  Memo3.Lines.Add('String (TValue): ' + Obj.valString.AsString);
+  {$IFNDEF JSX_NOVAR}
+  Memo3.Lines.Add('String (Variant): ' + Obj.valVariantString);
+  Memo3.Lines.Add('Double: ' + FloatToStr(Obj.valDouble));
+  {$ENDIF}
 
-  Log('ObjectType.var1: ' + Obj.ObjectType.Var1.ToString);
-  Log('IntfType.var1: ' + TSimpleObject(Obj.IntfType).Var1.ToString());
+  Memo3.Lines.Add('ObjectType.var1: ' + Obj.ObjectType.Var1.ToString);
+  Memo3.Lines.Add('IntfType.var1: ' + TSimpleObject(Obj.IntfType).Var1.ToString());
 
-  Log('VaLlist : ');
+  Memo3.Lines.Add('VaLlist : ');
   for var i in Obj.VaLlist do
-    Log('  ' + IntToStr(i.AsInteger));
+    Memo3.Lines.Add('  ' + IntToStr(i.AsInteger));
 
-  Log('IVaLlist : ');
+  Memo3.Lines.Add('IVaLlist : ');
   for var i in TIJX2ValueList(Obj.IVaLlist) do
-    Log(i.AsString);
+    Memo3.Lines.Add(i.AsString);
 
-  Log('ObjList : ');
+  Memo3.Lines.Add('ObjList : ');
   for var i in TJX2ObjList(Obj.ObjList) do
   begin
     var O := TSimpleObject(i);
-    Log('  ObjectType.var1: ' + o.var1.ToString);
+    Memo3.Lines.Add('  ObjectType.var1: ' + o.var1.ToString);
   end;
 
-  Log('StrValueDic : ');
+  Memo3.Lines.Add('StrValueDic : ');
   for var i in TJX2StrValueDic(Obj.StrValueDic) do
-    Log(' ' + i.Key + ': ' + IntToStr(TValue(i.Value).asInteger));
+    Memo3.Lines.Add(' ' + i.Key + ': ' + IntToStr(TValue(i.Value).asInteger));
 
-  Log('StrObjDic : ');
+  Memo3.Lines.Add('StrObjDic : ');
   for var i in TJX2StrObjDic(Obj.StrObjDic) do
   begin
     var o := TSimpleObject(i.Value);
-    Log(' ' + i.Key + ' : ' + o.var1.AsString + ', ' + o.var2.AsString);
+    Memo3.Lines.Add(' ' + i.Key + ' : ' + o.var1.AsString + ', ' + o.var2.AsString);
   end;
 
   Obj.Free;
 
 //----------------------------------------------------------------------------//
 
-  Log('Read JsonFile');
-  FS := TFileStream.Create('Beauty.json', fmOpenRead + fmShareDenyRead);
-  var RJson := FS.ReadRawString(TEncoding.UTF8);
+  Memo1.Lines.Add('Read Json file (Beauty.json)');
+  Memo4.Lines.Add('Read Json file (Beauty.json)');
+  FS := TFileStream.Create('Beauty.json', fmOpenRead + fmShareDenyNone);
+  Json := FS.ReadRawString(TEncoding.UTF8);
   FS.Free;
 
   Obj := W3DJSX2.Deserialize<TComplexObj>(Json);
-  Log( W3DJSX2.Serialize(Obj) );
+  Json := W3DJSX2.Serialize(Obj);
+  Memo1.Lines.Add( Json + '    Lenght: ' + Length(Json).ToString);
+  Memo4.Lines.Add( JsonBeautifier(Json) );
   Obj.Free;
 
 end;
 
-procedure TSimpleObject.SetVar1(v: TValue); begin Var1 := v; end;
-function TSimpleObject.GetVar1: TValue; begin Result := Var1; end;
+procedure TSimpleObject.SetVar1(v: TValue); begin Fvar1 := v; end;
+function TSimpleObject.GetVar1: TValue; begin Result := Fvar1; end;
 end.
 
